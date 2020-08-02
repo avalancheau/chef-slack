9c4570e798f4253d13cabaea47f8d0918d9bec9b
Kevin Yank <thatguy@kevinyank.com>
2020-08-02 13:21:31 +1000

    Output request URL on error

--- providers/say.rb
+++ providers/say.rb
@@ -4,20 +4,32 @@ end
 
 use_inline_resources
 
-module ShowUrlOnError
-  def say(connection, text="", options={})
-    @connection = connection
-    request      = Net::HTTP::Post.new(service_url)
-    request.body = encode_message(text, options)
-    response     = connection.http_request(request)
-    if response.code != "200"
-      raise Slackr::ServiceError, "#{service_url} - #{response.code} - #{response.body}"
+action :say do
+
+  module ::Slackr
+    module IncomingWebhook
+      extend self
+
+      attr_accessor :connection
+
+      # {
+      #   :formatter => [BasicFormatter, LinkFormatter, AttachmentFormatter]
+      # }
+      def say(connection, text="", options={})
+        @connection = connection
+        #formatter = options[:formatter] || BasicFormatter
+        #text      = format_text(formatter, text)
+        #TODO: fix law of demeter violations
+        request      = Net::HTTP::Post.new(service_url)
+        request.body = encode_message(text, options)
+        response     = connection.http_request(request)
+        if response.code != "200"
+          raise Slackr::ServiceError, "#{service_url} - #{response.code} - #{response.body}"
+        end
+      end
     end
   end
-end
 
-action :say do
-  Slackr.include ShowUrlOnError
   slack = Slackr.connect(node.slack.team,node.slack.api_key)
 
   options = {}
