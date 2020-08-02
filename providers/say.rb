397bd67457111906d19219e43b4821f82e4e13ec
Kevin Yank <thatguy@kevinyank.com>
2020-08-02 16:18:39 +1000

    Install Avalanche fork of Slackr gem

--- providers/say.rb
+++ providers/say.rb
@@ -5,31 +5,7 @@ end
 use_inline_resources
 
 action :say do
-
-  module ::Slackr
-    module IncomingWebhook
-      extend self
-
-      attr_accessor :connection
-
-      # {
-      #   :formatter => [BasicFormatter, LinkFormatter, AttachmentFormatter]
-      # }
-      def say(connection, text="", options={})
-        @connection = connection
-        #formatter = options[:formatter] || BasicFormatter
-        #text      = format_text(formatter, text)
-        #TODO: fix law of demeter violations
-        request      = Net::HTTP::Post.new(service_url)
-        request.body = encode_message(text, options)
-        response     = connection.http_request(request)
-        if response.code != "200"
-          raise Slackr::ServiceError, "#{service_url} - #{response.code} - #{response.body}"
-        end
-      end
-    end
-  end
-
+  require 'slackr'
   slack = Slackr.connect(node.slack.team,node.slack.api_key)
 
   options = {}
--- recipes/default.rb
+++ recipes/default.rb
@@ -16,12 +16,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+# Use Avalance fork of Slackr with fix for HTTP path including hostname
+git '/usr/local/src/slackr' do
+  repository 'https://github.com/avalancheau/slackr.git'
+end.run_action(:sync) # run at compile time, before chef_gem (below)
+execute 'gem build slackr.gemspec' do
+  cwd '/usr/local/src/slackr'
+end.run_action(:run) # run at compile time, before chef_gem (below)
+
 chef_gem "slackr" do
-  version '0.0.5'
+  version '0.0.6'
   action :install
   # removing compile_time warnings in Chef 12 per:
   # http://jtimberman.housepub.org/blog/2015/03/20/chef-gem-compile-time-compatibility/
   compile_time true if Chef::Resource::ChefGem.instance_methods(false).include?(:compile_time)
+  source "/usr/local/src/slackr/slackr-0.0.6.gem"
 end
-
-require 'slackr'
